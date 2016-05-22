2437994afb5d7e99fc1d19a081b3cb678cf6d89a
kishorsharma <brijkishor.cse@gmail.com>
2016-05-22 17:23:41 +0000

    Updated layout

--- js/freelancer.js
+++ js/freelancer.js
@@ -9,7 +9,7 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 30
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
